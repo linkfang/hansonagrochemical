2624da0780a58436a4b6d964bc4e11cbb424f92a
Link Zhou Fang <napsleep@outlook.com>
2020-04-22 12:37:20 -0400

    added news part completely

---
+++ data/news.js
@@ -0,0 +1,13 @@
+const newsData = [{
+        id: 1,
+        title: "China’s pesticide exports in 2020 will be lower than the past",
+        releaseDate: "April 18, 2020",
+        content: "China’s pesticide exports in 2020 will be lower than the past China’s pesticide exports were valued at 11.97 billion USD in 2019, which involved 1,104 active ingredients/combi, exported by 1,308 companies, targeted to 163 countries/regions. It is predicted that China’s pesticide export in 2020 will be compromised by the lowered production capacities, broken supply chains, and weaker demands from the key markets, such as the USA, EU, and Brazil, etc. For the detailed info, please email me at hanshuyou@gmail.com"
+    },
+    {
+        id: 2,
+        title: "感恩有大家守望！",
+        releaseDate: "April 19, 2020",
+        content: "感恩有大家守望！<br>北美疫情肆虐，到4月19日，美国感染人数近75万，死亡人数近3万，非常惨烈。加拿大这个人口小国，也有3.5万人感染，1,500多人逝去。死亡人数已超两次世界大战。经济停摆，生活受到了严重影响。<br>在这里，非常感谢合力科技的高峰先生，贸促会的樊利强先生及时给我寄来了口罩。这是雪中送炭，他们急别人所急，堪称道德楷模。他们的善举也强化了我为大家做好农药信息，登记，市场服务的信心和决心。<br>很多人问欧美国家政府为什么会给因Covid 19失去工作的人发放生活费？这是否会引起通胀？会不会稀释富人的财富？我认为，政府首先想到的是让人们活下去，不能出现因食物危机而引起的人道灾难。西方人和东方人不一样，他们很少存款，一般的家庭，在无工作收入的情形下，很难支撑3个月。现在，不是考虑通胀的时候，更不是考虑富人的感受的时间节点。西方的经济本质就是收入的稳定和消费信心的提振。如果人们没有收入，消费信心再受打击，整个经济运行就会坍塌了。昨天，美国联邦政府集体采购粮食和猪肉，赠送给食物银行Food Bank，免费发放给穷人。川普总统因此而备受称赞。<br>关于北美农业经济问题，对大田作物整体影响不大。农场主仍旧要种植油菜，大豆和玉米的。但是，农业投入品（inputs）可能会减量使用，原因是农场主手中现金流有限，收入预期和消费信心受到影响。我会紧密跟踪市场对大宗农药商品（如草甘膦，草铵膦）需求，及时给大家更新。对经济作物，如葡萄，水果等劳力密集型的产业，影响不小。原因是美加国门关闭了，来自墨西哥和越南等地的季节工人不能及时到位。<br>2019年，中国的农药出口基本和2018年持平，达119.7亿美元， 涉及到1,104个单一或者多个有效成分。共计有1,308家生产商/贸易商参与出口，产品覆盖163个国家和地区。2020年2月份，由于中国农药企业开工晚，原材料涨价，供应链受阻，欧美需求疲软等因素叠加，农药出口市值断崖下跌，草甘膦出口市值是2019年同期的一半左右。不可忽视的是，印度同期出口大增，在一定程度上弥补了市场的空缺。可以肯定的说，2020年中国农药出口市值会下跌的。建议在这段困难时刻，农药企业要苦练内功，加强管理，节约开支，谋划开拓渗透国际和国内两个市场，为2021起飞做好心理和物质准备。困难是暂时的，希望是永恒的，我们对农药的未来充满期待和希冀。<br>由于目前加拿大处于紧急状态，大家一般足不出户，在家工作。我在工作之余，沿着后院的小河散步，偶尔拍了几张渥太华的春天，分享给大家 <a href='http://www.hansonagrochemical.com/shots.html'>http://www.hansonagrochemical.com/shots.html</a>，希望喜欢。"
+    }
+]
--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
 let han={
-    init: function(){
-        document.querySelector("#humburger").addEventListener("click", ()=>{
+    init: ()=>{
+        document.getElementById("humburger").addEventListener("click", ()=>{
             document.querySelector(".navHum").classList.toggle("disappear");
         })
 
         document.querySelector(".header").addEventListener("mouseleave", ()=>{
             document.querySelector(".navHum").classList.add("disappear");
         })
+
+        han.addListeners()
+    },
+
+    addListeners: ()=>{
+        document.getElementById("overlay").addEventListener("click", han.hideModalNews)
+        document.getElementById("closeModalBtn").addEventListener("click", han.hideModalNews)
+
+        let newsList = document.getElementById("newsList")
+        let newsArray = newsList.querySelectorAll(".newsItem")
+        newsArray.forEach( item => item.addEventListener("click", han.showNews))
+    },
+
+    showNews: (ev)=>{
+        document.getElementById("overlay").className = ""
+        document.getElementById("modalNews").className = ""
+
+        let id = ev.currentTarget.getAttribute("data-id")
+        let currentNews = newsData.find( item => item.id == id)
+
+        document.getElementById("newsTitle").textContent = currentNews.title
+        document.getElementById("newsDate").textContent = currentNews.releaseDate
+        document.getElementById("newsContent").innerHTML = currentNews.content
+    },
+
+    hideModalNews: ()=>{
+        document.getElementById("overlay").className = "hide"
+        document.getElementById("modalNews").className = "hide"
     }
 }
 
