a44545c7758e6e9976756781c07f4b193c2f8023
Link Zhou Fang <napsleep@outlook.com>
2020-05-01 13:46:47 -0400

    adde one more news

--- data/news.js
+++ data/news.js
@@ -13,10 +13,17 @@ const newsData = [{
         url: ""
     },
     {
-        id:3,
+        id: 3,
         title: "Chinese pesticide exports down in the first two months of 2020",
         releaseDate: "April 29, 2020",
         content: "Chinese pesticide exports down in the first two months of 2020 Shuyou Han China’s pesticide exports during the first two months of 2020 have been hampered by the spread of the coronavirus, Covid-19.",
         url: "https://agrow.agribusinessintelligence.informa.com/AG032677/Chinese-pesticide-exports-down-in-first-two-months-of-2020"
+    },
+    {
+        id: 4,
+        title: "Agrochemical Patent Watch 2019 Shuyou Han During 2019",
+        releaseDate: "May 1, 2020",
+        content: "Agrochemical Patent Watch 2019 Shuyou Han During 2019, a total of 6,600 novel crop protection-relevant patents were published on Espacenet, the European Patent Office. The following text and graphs summarise patents published by geological distribution, major applicants and chemical function. 1. Published patents by continent and country At the continent level, Asia was the innovation powerhouse for crop protection products. Out of 6,600 patents, Asia accounted for half of the filed patents with 3,258, followed by Europe with 1,260 patents (making up 19%), North America with 918 (14%), South America with 809 (12%), Oceania with 269 (3%) and Africa with 86 (1%). <a href='https://lnkd.in/gvvMyJR'>https://lnkd.in/gvvMyJR</a> 2019年全球农药专利观察，从专利地理分布，主要申请者和化合物分类等方面做了详尽综述 <a href='https://lnkd.in/ghsTmun'>https://lnkd.in/ghsTmun</a> ",
+        url: "https://www.linkedin.com/posts/activity-6661964957237809154-E9m6"
     }
 ]
